e1900b503b4bc94c01297a439df65d6ff45fed29
Uchencho <aloziekelechi17@gmail.com>
2020-08-20 16:06:06 +0100

    deleting a user

--- backoffice/api/urls.py
+++ backoffice/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AllUsers
+from .views import AllUsers, DeleteUser
 
 app_name = "backoffice"
 
 urlpatterns = [
     path('users', AllUsers.as_view(), name='users'),
+    path('users/<int:pk>', DeleteUser.as_view(), name='deleteUser'),
 ]
--- backoffice/api/views.py
+++ backoffice/api/views.py
@@ -7,3 +7,12 @@ class AllUsers(generics.ListAPIView):
     queryset                = User.objects.all()
     serializer_class        = UserAdminSerializer
     permission_classes      = [permissions.IsAdminUser]
+
+
+class DeleteUser(generics.DestroyAPIView):
+    queryset                = User.objects.all()
+    serializer_class        = UserAdminSerializer
+    permission_classes      = [permissions.IsAdminUser]
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
